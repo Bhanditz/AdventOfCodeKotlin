1c0466dec566faed28c31ceded813dde10053fdc
Francois Campbell <campbell.francois@gmail.com>
2016-02-21 16:40:26 -0500

    Updated to Kotlin 1.0.0

---
+++ src/main/kotlin/com/github/marcospereira/day10/Day10.kt
@@ -0,0 +1,16 @@
+package com.github.marcospereira.day10
+
+import com.github.marcospereira.Day
+
+/**
+ * Created by francois on 2016-02-21.
+ */
+class Day10 : Day() {
+    override fun part1(): Any? {
+        throw UnsupportedOperationException()
+    }
+
+    override fun part2(): Any? {
+        throw UnsupportedOperationException()
+    }
+}
